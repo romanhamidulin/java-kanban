bdedde7d2ccf58967eee5c8844acd1568a8a1bd5
Хамидулин Роман Родионович <romanhamidulin@ya.ru>
2025-05-12 11:42:00 +0500

    CheckStyle правки

--- src/ru/yandex/practicum/manager/HistoryManager.java
+++ src/ru/yandex/practicum/manager/HistoryManager.java
@@ -2,11 +2,13 @@ package ru.yandex.practicum.manager;
 
 import ru.yandex.practicum.tasks.Task;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 }
