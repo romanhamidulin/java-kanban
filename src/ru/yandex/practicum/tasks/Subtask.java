57b1b0265c559ca4513ddfd625bda7364c89a481
Хамидулин Роман Родионович <romanhamidulin@ya.ru>
2025-05-12 11:50:43 +0500

    CheckStyle правки

--- src/ru/yandex/practicum/tasks/Subtask.java
+++ src/ru/yandex/practicum/tasks/Subtask.java
@@ -7,12 +7,11 @@ public class Subtask extends Task {
     private final int epicId;
 
     public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
-
         super(id, name, description, status);
         this.epicId = epicId;
     }
-    public Subtask(String name, String description, int epicId) {
 
+    public Subtask(String name, String description, int epicId) {
         super(name, description);
         this.epicId = epicId;
     }
