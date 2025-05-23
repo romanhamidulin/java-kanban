8bea4a4201fe62b2a60a1cb5090468610a595dfb
Хамидулин Роман Родионович <romanhamidulin@ya.ru>
2025-05-23 15:21:28 +0500

    Правки

--- src/ru/yandex/practicum/manager/FileBackedTaskManager.java
+++ src/ru/yandex/practicum/manager/FileBackedTaskManager.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.manager;
 
+import ru.yandex.practicum.enums.TaskStatus;
 import ru.yandex.practicum.tasks.Epic;
 import ru.yandex.practicum.tasks.Subtask;
 import ru.yandex.practicum.tasks.Task;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
     private final File file;
@@ -73,6 +75,31 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         save();
     }
 
+    public void deleteTask(Integer taskId) {
+        super.deleteTask(taskId);
+        save();
+    }
+
+    // удалить эпик по id
+    @Override
+    public void deleteEpic(Integer epicId) {
+        super.deleteEpic(epicId);
+        save();
+    }
+
+    // удалить подзадачу по id
+    @Override
+    public void deleteSubtask(Integer subTaskId) {
+        super.deleteSubtask(subTaskId);
+        save();
+    }
+
+    @Override
+    public void deleteSubtask(Subtask subtask) {
+        super.deleteSubtask(subtask);
+        save();
+    }
+
     private void save() {
         try (FileWriter writer = new FileWriter(file, false)) {
             writer.write("id,type,name,status,description,epic\n");
@@ -104,4 +131,48 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         );
     }
 
+    // Метод загрузки из файла
+    public static FileBackedTaskManager loadFromFile(File file) {
+        FileBackedTaskManager manager = new FileBackedTaskManager(file);
+        try (BufferedReader buf = new BufferedReader(new FileReader(file))) {
+            String line;
+            while (buf.ready()) {
+                line = buf.readLine();
+                if (line.contains("EPIC")) {
+                    Task epic = fromString(line);
+                    manager.addEpic((Epic) epic);
+                } else if (line.contains("SUBTASK")) {
+                    Task subtask = fromString(line);
+                    manager.addSubtask((Subtask) subtask);
+                } else {
+                    Task task = fromString(line);
+                    manager.addTask(task);
+                }
+            }
+        } catch (IOException e) {
+            throw new ManagerSaveException("Ошибка загрузки задачи из файла", e);
+        }
+        return manager;
+    }
+
+    private static Task fromString(String value) {
+        String[] parts = value.split(",");
+        int id = Integer.parseInt(parts[0]);
+        String type = parts[1];
+        String name = parts[2];
+        TaskStatus status = TaskStatus.valueOf(parts[3]);
+        String description = parts[4];
+        int epicId = parts.length > 5 ? Integer.parseInt(parts[5]) : 0;
+
+        switch (type) {
+            case "TASK":
+                return new Task(id, name, description, status);
+            case "EPIC":
+                return new Epic(id, name, description, status);
+            case "SUBTASK":
+                return new Subtask(id, name, description, status, epicId);
+            default:
+                throw new IllegalArgumentException("Неизвестный типа задачи: " + type);
+        }
+    }
 }
